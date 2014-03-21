fb64de9524a2b0c3473f4dc66739740df2438f63
Neil Mithipati <neil.mithipati@gmail.com>
2014-03-21 10:14:23 -0500

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Foodtruck::Application.config.secret_key_base = 'ae0cbd529a7d2001b919a636e65e309e673800b72a3ea94943052fc70313c8e10dc3357b30b16803e2b203f10297253e2f3cf15df22c64e76a968dc98be144de'
